5e7c3b55f6874cfdf6cb4d89ca041c4365af3c9e
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-05 15:31:45 +0900

    Add Company Information

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Grid, List } from '@mui/material'
+import { Box, Container, Divider, Grid, List, Typography } from '@mui/material'
 
 import React from 'react'
 
@@ -41,6 +41,22 @@ export default function Footer (): JSX.Element {
             </List>
           </Grid>
         </Grid>
+        <Typography variant="body2" align="center" color="text.secondary" sx={{ p: 1 }}>
+          사업자 등록번호 846-88-00665 | 통신판매업 신고번호 제 2020-전남순천-2995 호
+          <br />
+          전남 순천시 우석로 7, 청암대학교 청암건강교육센터 3층, (주)빌리버 | 대표번호 070-4227-9371
+        </Typography>
+
+        <Typography
+          variant="body2"
+          sx={{
+            color: 'text.secondary',
+            textAlign: 'center',
+            p: 1
+          }}
+        >
+          © BELIVVR All Rights Reserved.
+        </Typography>
       </Container>
     </Box>
   )
