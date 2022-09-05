2793e290bf41c0ca3004adf72220f86a747f2ce5
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-05 15:04:17 +0900

    ListButton.test Delete unnecessary things

--- src/components/Footer/ListButton.test.tsx
+++ src/components/Footer/ListButton.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import React from 'react'
 
@@ -7,25 +7,5 @@ import ListButton from './ListButton'
 describe('<ListButton/>', () => {
   it('renders ListButton Component', () => {
     render(<ListButton text="test1" link="/" />)
-  })
-
-  it('Is ListItem working', () => {
-    render(
-      <ListButton
-        text="이용약관"
-        link="https://www.naver.com/"
-        icon="https://play-lh.googleusercontent.com/yPtnkXQAn6yEahOurxuYZL576FDXWn3CqewVcEWJsXlega_nSiavBvmaXwfTGktGlQ=w240-h480-rw"
-      />
-    )
-
-    const textElement = screen.getByText('이용약관')
-    const iconElement = screen.getByText(
-      'https://play-lh.googleusercontent.com/yPtnkXQAn6yEahOurxuYZL576FDXWn3CqewVcEWJsXlega_nSiavBvmaXwfTGktGlQ=w240-h480-rw'
-    )
-
-    expect(textElement).toBeInTheDocument()
-    fireEvent.click(textElement)
-
-    expect(iconElement).toBeInTheDocument()
   })
 })
