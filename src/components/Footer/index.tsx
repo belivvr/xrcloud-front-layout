c649a66544d6c97418cefcc1dff8c762119afcb4
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-09-06 10:53:11 +0900

    Modify Footer
    styled 컴포넌트에서 Item,ItemList를 불러와 코드를 불러옵니다.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,17 @@
-import { Box, Container, Divider, Grid, List, Typography } from '@mui/material'
-
 import React from 'react'
 
-import ListButton from './ListButton'
-
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+  ListItemIcon
+} from '@mui/material'
 import GitHubIcon from '@mui/icons-material/GitHub'
 import ForumIcon from '@mui/icons-material/Forum'
+
+import { Item, ItemList } from './styled'
 
 export default function Footer (): JSX.Element {
   return (
@@ -13,35 +19,32 @@ export default function Footer (): JSX.Element {
       <Divider />
       <Container sx={{ padding: 2 }}>
         <Grid container columns={{ md: 12 }} spacing={{ md: 4 }}>
-          <Grid item md={4}>
-            <List>
-              <ListButton text="이용약관" link="/" />
-              <ListButton text="개인정보처리방침" link="/" />
-              <ListButton text="회사 정보" link="/" />
-            </List>
-          </Grid>
-          <Grid item md={4}>
-            <List dense>
-              <ListButton
-                text="WebXRCloud"
-                link="https://www.facebook.com/groups/webxrko"
-                icon={<ForumIcon />}
-              />
-              <ListButton
-                text="Belivvr Github"
-                link="https://github.com/belivvr"
-                icon={<GitHubIcon />}
-              />
-            </List>
-          </Grid>
-          <Grid item md={4}>
-            <List>
-              <ListButton text="회사 소개" link="https://about.belivvr.com" />
-              <ListButton text="문의하기" link="mailto:connect@belivvr.com" />
-            </List>
-          </Grid>
+          <ItemList>
+            {/* TODO: 필요한 url 넣기 */}
+            <Item href="/">이용약관</Item>
+            <Item href="/">개인정보처리방침</Item>
+            <Item href="/">회사 정보</Item>
+          </ItemList>
+          <ItemList dense>
+            <Item href="https://www.facebook.com/groups/webxrko">
+              <ListItemIcon>
+                <ForumIcon />
+              </ListItemIcon>
+              WebXR Korea
+            </Item>
+            <Item href="https://github.com/belivvr">
+              <ListItemIcon>
+                <GitHubIcon />
+              </ListItemIcon>
+              Belivvr Github
+            </Item>
+          </ItemList>
+          <ItemList>
+            <Item href="https://about.belivvr.com">회사 소개</Item>
+            <Item href="mailto:connect@belivvr.com">문의하기</Item>
+          </ItemList>
         </Grid>
-        <Typography variant="body2" align="center" color="text.secondary" sx={{ p: 1 }}>
+        <Typography variant="body2" align="center" color="text.secondary" sx={{ padding: 1 }}>
           사업자 등록번호 846-88-00665 | 통신판매업 신고번호 제 2020-전남순천-2995 호
           <br />
           전남 순천시 우석로 7, 청암대학교 청암건강교육센터 3층, (주)빌리버 | 대표번호 070-4227-9371
@@ -52,7 +55,7 @@ export default function Footer (): JSX.Element {
           sx={{
             color: 'text.secondary',
             textAlign: 'center',
-            p: 1
+            padding: 1
           }}
         >
           © BELIVVR All Rights Reserved.
